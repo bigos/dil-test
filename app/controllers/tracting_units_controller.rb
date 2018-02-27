7959cb1d9615860b7f40521284f9b19e703d1e17
Jacek Podkanski <ruby.object@googlemail.com>
2018-02-27 00:49:56 +0000

    got the table with data

--- app/controllers/tracting_units_controller.rb
+++ app/controllers/tracting_units_controller.rb
@@ -1,7 +1,16 @@
 class TractingUnitsController < ApplicationController
   def index
+    @tracking_units = TrackingUnit.paginate(page: params[:page])
   end
 
   def show
+  end
+
+  private
+
+  # only allow white listed parameters
+
+  def person_params
+    params.require(:tracking_unit).permit(:page)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'tracting_units/show'
 
+  root to: 'tracting_units#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
