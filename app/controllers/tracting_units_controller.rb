03fc208e38f6c39954a2c7e57d353ed90533ca83
Jacek Podkanski <ruby.object@googlemail.com>
2018-02-27 01:24:51 +0000

    time for bed

--- app/controllers/tracting_units_controller.rb
+++ app/controllers/tracting_units_controller.rb
@@ -1,5 +1,16 @@
 class TractingUnitsController < ApplicationController
+
+  # finish me
+  # https://github.com/activerecord-hackery/ransack
+  # https://github.com/activerecord-hackery/ransack_demo/blob/master/app/models/user.rb
+
   def index
+    @q = TrackingUnit.ransack(params[:q])
+
+
+    # or use `to_a.uniq` to remove duplicates (can also be done in the view):
+    @tracking_units = @q.result.includes(:articles).page(params[:page]).to_a.uniq
+
     @tracking_units = TrackingUnit.paginate(page: params[:page])
   end
 
