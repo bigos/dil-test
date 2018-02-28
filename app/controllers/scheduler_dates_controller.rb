ae5ab1b305615f61da7e2e383d73422d5dab4741
Jacek Podkanski <ruby.object@googlemail.com>
2018-02-28 15:32:06 +0000

    some progress

--- app/controllers/scheduler_dates_controller.rb
+++ app/controllers/scheduler_dates_controller.rb
@@ -1,4 +1,11 @@
 class SchedulerDatesController < ApplicationController
   def index
+
+    @q = SchedulerDate.ransack(params[:q])
+
+
+    # or use `to_a.uniq` to remove duplicates (can also be done in the view):
+    @scheduler_dates = @q.result.includes(:projects_tracking_unit)
+                        .paginate(page: params[:page], per_page: 10)
   end
 end
