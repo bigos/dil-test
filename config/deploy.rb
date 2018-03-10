f3e217c90a0f8be34b94095403e59cb57ea8f181
Jacek Podkanski <ruby.object@googlemail.com>
2018-03-10 15:27:03 +0000

    deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock '~> 3.10.1'
 
 set :secrets, YAML.load_file("#{File.dirname(__FILE__)}/secrets.yml")
 
-set :application, "dil-test"
-set :repo_url, "git@github.com:bigos/dil-test.git"
+set :application, 'dil-test'
+set :repo_url, 'git@github.com:bigos/dil-test.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/rails/rails_project"
+set :deploy_to, '/home/rails/rails_project'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -25,10 +25,10 @@ set :pty, true
 set :format, :pretty
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
