5ad6d98a327474c26e89b52a0f9ef3dbad66f0b9
Jacek Podkanski <ruby.object@googlemail.com>
2018-03-11 00:30:41 +0000

    progress with deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
-       'public/system', 'vendor/bundle'
+       'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
